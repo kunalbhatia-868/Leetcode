6ed06e2f19f09c1949170dc7b82c13fdf6fca431
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2022-10-31 17:21:46 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -6,46 +6,26 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-    int bst(vector<vector<int>>& arr,int r)
-    {
-        int l=0;
-        int h=arr[0].size()-1;
-        int first1=-1;
-        while(l<=h)
-        {
-            int mid=l+(h-l)/2;
-            if(arr[r][mid]==1)
-            {
-                first1=mid;
-                h=mid-1;
-            }
-            else
-            {
-                l=mid+1;
-            }
-        }
-        
-        if(first1==-1){
-            return 0;
-        }
-        return arr[0].size()-first1;
-    }
-    
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
 	    // code here
-	    int maxOnes=0,maxRow=-1;
-	    
-	    for(int i=0;i<n;i++)
+	    int i=0,j=m-1;
+	    int max1=0,row=-1;
+	    while(i<n && j>=0)
 	    {
-	        int rowOnes=bst(arr,i);
-	        
-	        if(rowOnes>maxOnes){
-	            maxOnes=rowOnes;
-	            maxRow=i;
+	        if(arr[i][j]==0)
+	        {
+	            i++;
+	        }
+	        else
+	        {
+	            if(m-j>max1){
+	                max1=m-j;
+	                row=i;
+	            }
+	            j--;
 	        }
 	    }
-	    
-	    return maxRow;
+	    return row;
 	}
 
 };
