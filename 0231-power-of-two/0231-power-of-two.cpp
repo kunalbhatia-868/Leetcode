fd6c2dce3792a73220f19772a5f84e96cd239dc8
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2022-11-09 16:35:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (28.12%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        double ans=log2(n);
+        return ans==(int)ans;
+    }
+};
