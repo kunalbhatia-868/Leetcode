2154908261457787acdffb7c6b7b963aafc6225d
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2022-11-25 23:53:56 +0530

    Time: 70 ms (69.15%), Space: 20.6 MB (12.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-     static bool comp(vector<int>& a,vector<int>& b)
-    {
+    static bool comp(vector<int>& a,vector<int>& b){
         return a[0]<b[0];
     }
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> output;
+        stack<vector<int>> st;    
         sort(intervals.begin(),intervals.end(),comp);
-   
-        int start=intervals[0][0];
-        int end=intervals[0][1];
         
-        int i=1;
-        while(i<intervals.size())
+        for(int i=0;i<intervals.size();i++)
         {
-            if(intervals[i][0]>end)
-            {
-                output.push_back({start,end});
-                start=intervals[i][0];                
-                end=intervals[i][1];
-
+            if(st.empty() || st.top()[1]<intervals[i][0]){
+                st.push(intervals[i]);
             }
-            else
-            {
-                end=max(end,intervals[i][1]);
+            else{
+                //merge
+                int start=st.top()[0];
+                int end=st.top()[1];
+                st.pop();
+                st.push({start,max(end,intervals[i][1])});
             }
-            i++;
         }
-        output.push_back({start,end});
+        
+        while(!st.empty()){
+            output.push_back(st.top());
+            st.pop();
+        }
+        
         return output;
     }
 };
