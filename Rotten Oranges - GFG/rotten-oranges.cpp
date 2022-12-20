e637cc430e159701b47b06ef478eeadac4092860
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2022-12-20 15:46:42 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -13,13 +13,13 @@ class Solution
         int m=grid[0].size();
         queue<pair<pair<int,int>,int>> q;
         
-        vector<vector<int>> vis(n,vector<int>(m,0));
+        // vector<vector<int>> vis(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
-                    vis[i][j]=1;
+                    // vis[i][j]=1;
                 }
             }
         }
@@ -37,10 +37,10 @@ class Solution
                 int r=frow+coordinates[i][0];
                 int c=fcol+coordinates[i][1];
     
-                if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1 && vis[r][c]==0){
+                if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1 ){
                     q.push({{r,c},ftime+1});
                     grid[r][c]=2;
-                    vis[r][c]=1;
+                    // vis[r][c]=1;
                 }
             }
         }
