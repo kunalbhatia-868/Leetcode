83a69905113b05c62be7506d472f3d470c66efbb
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2022-12-29 20:15:37 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,34 +8,26 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    // Memoized Solution doesn't give TLE  
-    int knapSack(int W, int wt[], int val[], int n,vector<vector<int>>& dp) 
-    { 
-       // Your code here
-       if(n==0 || W==0){
-           return 0;
-       }
-       
-       if(dp[n][W]!=-1){
-           return dp[n][W];
-       }
-       
-       int unPickedProfit=knapSack(W,wt,val,n-1,dp);
-       
-       if(wt[n-1]<=W){
-           int pickedProfit=knapSack(W-wt[n-1],wt,val,n-1,dp);
-           dp[n][W]=max(unPickedProfit,val[n-1]+pickedProfit);
-       }
-       else{
-           dp[n][W]=unPickedProfit;
-       }
-       return dp[n][W];
-    }
+    // DP Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n+1,vector<int>(W+1,-1));
-       knapSack(W,wt,val,n,dp);
+       int dp[n+1][W+1];
+       for(int i=0;i<n+1;i++){
+           for(int j=0;j<W+1;j++){
+               if(i==0 || j==0){
+                   dp[i][j]=0;
+               }
+               else{
+                   if(wt[i-1]<=j){
+                       dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                   }
+                   else{
+                       dp[i][j]=dp[i-1][j];
+                   }
+               }
+           }
+       }
        return dp[n][W];
     }
 };
