9ffc04d1fb6063fc798e824520568d5a268402cb
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2023-01-06 15:06:27 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,30 +9,26 @@ using namespace std;
 
 class Solution{
 public:
-    int knapsack(int val[], int wt[],int N, int W,vector<vector<int>>& dp)
-    {
-        if(W==0 || N==0){
-            return 0;    
-        }
-        
-        if(dp[N][W]!=-1){
-            return dp[N][W];
-        }
-        
-        if(wt[N-1]<=W){
-            dp[N][W]= max(knapsack(val,wt,N-1,W,dp),val[N-1]+knapsack(val,wt,N,W-wt[N-1],dp));
-        }
-        else{
-            dp[N][W]= knapsack(val,wt,N-1,W,dp);
-        }
-        
-        return dp[N][W];
-    }
+    
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
         vector<vector<int>> dp(N+1,vector<int>(W+1,-1));
-        knapsack(val,wt,N,W,dp);
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<W+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+                else{
+                    if(wt[i-1]<=j){
+                        dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i][j-wt[i-1]]);
+                    }
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }
+                }
+            }
+        }
         return dp[N][W];
     }
 };
