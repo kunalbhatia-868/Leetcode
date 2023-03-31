ef4882f3ba5abbd3913ecaf76e371ae766b8b3b4
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2023-03-31 17:09:14 +0530

    Time: 11 ms (91.82%), Space: 13.3 MB (78.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool row=false,col=false;
-        for(int  i=0;i<matrix[0].size();i++)
-        {
-            if(matrix[0][i]==0)
-            {
-                row=true;
-            }
-        }
-        for(int  i=0;i<matrix.size();i++)
-        {
-            if(matrix[i][0]==0)
-            {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int row=false;
+        int col=false;
+        
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
                 col=true;
             }
         }
+        for(int i=0;i<m;i++){
+            if(matrix[0][i]==0){
+                row=true;
+            }
+        }
         
-        for(int i=1;i<matrix.size();i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=1;j<matrix[0].size();j++)
+            for(int j=1;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
-            }   
+            }
         }
         
-        for(int i=1;i<matrix.size();i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=1;j<matrix[0].size();j++)
+            for(int j=1;j<m;j++)
             {
                 if(matrix[i][0]==0 || matrix[0][j]==0)
                 {
                     matrix[i][j]=0;
                 }
-            }   
-        }
-        
-        if(row){
-            for(int i=0;i<matrix[0].size();i++)
-            {
-                matrix[0][i]=0;
             }
         }
-        if(col){
-            for(int i=0;i<matrix.size();i++)
-            {
+        
+        if(col)
+        {
+            for(int i=0;i<n;i++){
                 matrix[i][0]=0;
             }
         }
+        if(row){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+        
     }
 };
