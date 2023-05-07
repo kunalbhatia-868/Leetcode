86a020e5da83484e840d6abb570858835e602fdb
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2023-05-07 21:07:35 +0530

    Time: 471 ms (100.00%), Space: 200.3 MB (57.14%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.cpp
@@ -0,0 +1,60 @@
+class FrequencyTracker {
+public:
+    unordered_map<int,int> mp;
+    unordered_map<int,int> freq;
+    
+    FrequencyTracker() {
+        mp.clear();
+        freq.clear();
+    }
+    
+    void add(int number) {
+        if(mp.count(number)==0){
+            mp[number]=1;
+            freq[1]++;
+        }
+        else{
+            freq[mp[number]]-=1;
+            if(freq[mp[number]]==0){
+                freq.erase(mp[number]);
+            }
+            
+            mp[number]+=1;
+            freq[mp[number]]+=1;  
+        }
+    }
+    
+    void deleteOne(int number) {
+        if(mp.count(number)==0){
+            return ;
+        }
+        else{
+            freq[mp[number]]-=1;
+            if(freq[mp[number]]==0){
+                freq.erase(mp[number]);
+            }
+            mp[number]-=1;
+            if(mp[number]==0){
+                mp.erase(number);
+            }
+            
+            if(mp.count(number)>0){ // Check if the new frequency exists in the freq map
+                freq[mp[number]]+=1; // Increment its frequency count
+            }   
+            
+            
+        }
+    }
+    
+    bool hasFrequency(int frequency) {
+        return freq.count(frequency);
+    }
+};
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker* obj = new FrequencyTracker();
+ * obj->add(number);
+ * obj->deleteOne(number);
+ * bool param_3 = obj->hasFrequency(frequency);
+ */
