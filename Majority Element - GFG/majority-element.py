3601a4618caeb18bbffaa5c59c987d3448075501
Kunal Bhatia <61620329+kunalbhatia-868@users.noreply.github.com>
2023-05-11 23:40:51 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.py
+++ Majority Element - GFG/majority-element.py
@@ -18,26 +18,26 @@
 class Solution:
     def majorityElement(self, A, N):
         #Your code here
-        if N==1:
-            return A[0]
-        
-        A.sort()
-        
         count=1
         curr=A[0]
-        for idx in range(1,N):
-            if A[idx]==curr:
+        
+        for i in range(1,N):
+            if(A[i]==curr):
                 count+=1
             else:
+                count-=1
+            
+            if count==0:
                 count=1
-                curr=A[idx]
-            
-            if count>N/2:
-                return curr
-            
+                curr=A[i]
+                
         
-        return -1 
+        occ=0
+        for i in range(N):
+            if A[i]==curr:
+                occ+=1
         
+        return curr if occ>N/2 else -1
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
